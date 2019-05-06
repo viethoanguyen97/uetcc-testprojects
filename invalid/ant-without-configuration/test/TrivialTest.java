f53a621c4ddd1ece741bf74642274b77bfb5eafe
Viet Hoa Nguyen <viethoanguyen97@gmail.com>
2019-05-06 13:35:07 +0700

    first commit

---
+++ invalid/ant-without-configuration/test/TrivialTest.java
@@ -0,0 +1,19 @@
+
+import fi.helsinki.cs.tmc.edutestutils.Points;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+@Points("trivial")
+public class TrivialTest {
+    @Test
+    public void testF() {
+        Trivial t = new Trivial();
+        assertEquals(1, t.f());
+    }
+    
+    @Test
+    public void testA() {
+        Trivial t = new Trivial();
+        assertEquals(1, 2);
+    }
+}
